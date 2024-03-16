3fa35f22fb6a9149cba72296fa40371e32f009f0
andhitanh <onecodhita@gmail.com>
2024-03-16 21:46:18 +0700

    feat : bezier divide and conquer step by step

--- src/bezier_dnc.py
+++ src/bezier_dnc.py
@@ -6,7 +6,7 @@ def find_midpoint(point1, point2):
     return midpoint
 
 def bezier_dnc(point1, point2, point3, iteration, bezierpoint) :
-    if iteration > 0 :
+    if iteration > 0 : # rekurens
         midpoint1 = find_midpoint(point1, point2)
         midpoint2 = find_midpoint(point2, point3)
         midpoint3 = find_midpoint(midpoint1, midpoint2)
@@ -16,7 +16,7 @@ def bezier_dnc(point1, point2, point3, iteration, bezierpoint) :
         bezier_dnc(point1, midpoint1, midpoint3, iteration, bezierpoint)
         bezierpoint.append(midpoint3)
         bezier_dnc(midpoint3, midpoint2, point3, iteration, bezierpoint)
-    elif iteration == 0 :
+    elif iteration == 0 : # basis
         return
 
 def bezier_dnc_final_curve(point1, point2, point3, iteration) :
@@ -26,7 +26,7 @@ def bezier_dnc_final_curve(point1, point2, point3, iteration) :
     bezier_curve_points.append(point3)
     return bezier_curve_points
 
-control_points = [[1,2], [3,4], [5,7]]
-iteration = int(input("iter : "))
-bezier = bezier_dnc_final_curve(control_points[0], control_points[1], control_points[2], iteration)
-print(bezier)
+# control_points = [[1,2], [3,4], [5,7]]
+# iteration = int(input("iter : "))
+# bezier = bezier_dnc_final_curve(control_points[0], control_points[1], control_points[2], iteration)
+# print(bezier)
