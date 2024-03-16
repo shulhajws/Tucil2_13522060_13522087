9b0291cdc1e2dd04d6383135f244eccee30d5dc0
andhitanh <onecodhita@gmail.com>
2024-03-16 23:13:52 +0700

    update bezier divide and conquer

--- src/bezier_dnc.py
+++ src/bezier_dnc.py
@@ -1,32 +1,36 @@
 def find_midpoint(point1, point2):
     """Returning the midpoint of two points"""
-    midpoint = []
-    for i in range(2) :
-        midpoint.append(0.5*point1[i] + 0.5*point2[i])
+    midpoint = [(point1[0] + point2[0]) / 2, (point1[1] + point2[1]) / 2]
     return midpoint
 
-def bezier_dnc(point1, point2, point3, iteration, bezierpoint) :
-    if iteration > 0 : # rekurens
+def bezier_dnc(point1, point2, point3, iteration):
+    bezierpoint = []  
+    if iteration > 0:  
         midpoint1 = find_midpoint(point1, point2)
         midpoint2 = find_midpoint(point2, point3)
         midpoint3 = find_midpoint(midpoint1, midpoint2)
 
         iteration -= 1
 
-        bezier_dnc(point1, midpoint1, midpoint3, iteration, bezierpoint)
+        left_points = bezier_dnc(point1, midpoint1, midpoint3, iteration)  # Recursively get points for left part
+        bezierpoint.extend(left_points)
         bezierpoint.append(midpoint3)
-        bezier_dnc(midpoint3, midpoint2, point3, iteration, bezierpoint)
-    elif iteration == 0 : # basis
-        return
+        right_points = bezier_dnc(midpoint3, midpoint2, point3, iteration)  # Recursively get points for right part
+        bezierpoint.extend(right_points)
 
-def bezier_dnc_final_curve(point1, point2, point3, iteration) :
-    bezier_curve_points = []
-    bezier_curve_points.append(point1)
-    bezier_dnc(point1, point2, point3, iteration, bezier_curve_points)
-    bezier_curve_points.append(point3)
-    return bezier_curve_points
+    return bezierpoint  
 
-# control_points = [[1,2], [3,4], [5,7]]
-# iteration = int(input("iter : "))
-# bezier = bezier_dnc_final_curve(control_points[0], control_points[1], control_points[2], iteration)
-# print(bezier)
+def bezier_dnc_final_curve(control_points, iteration):
+    all_bezier_curves = []
+    for i in range(1, iteration + 1):
+        bezier_curve_points = []
+        bezier_curve_points.append(control_points[0])
+        bezier_curve_points.extend(bezier_dnc(control_points[0], control_points[1], control_points[2], i))
+        bezier_curve_points.append(control_points[2])
+        all_bezier_curves.append(bezier_curve_points)
+    return all_bezier_curves
+
+control_points = [[1, 2], [3, 4], [5, 7]]
+iteration = 4
+all_bezier_curves = bezier_dnc_final_curve(control_points, iteration)
+print(all_bezier_curves)
